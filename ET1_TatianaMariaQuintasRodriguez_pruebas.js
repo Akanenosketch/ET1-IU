4287dea16d4a02e64534e0e310cc052485c14e76
Akanenosketch <90091466+Akanenosketch@users.noreply.github.com>
2024-10-11 18:52:41 +0200

    Started to add stuff in pruebas

--- ET1_TatianaMariaQuintasRodriguez_pruebas.js
+++ ET1_TatianaMariaQuintasRodriguez_pruebas.js
@@ -1,3 +1,30 @@
 pruebasunitarias_=Array(
+    // analysis_preparation
+    //id_analysys_prepraration
+    Array(1, 'analysis_preparation', 'id_analysis_preparation', 1, '', false),
+    Array(1, 'analysis_preparation', 'id_analysis_preparation', 2, 123456, true),
+    Array(2, 'analysis_preparation', 'id_analysis_preparation', 3, 12345678901245, false ),
+    Array(2, 'analysis_preparation', 'id_analysis_preparation', 4, 12345678, true),
+    Array(3, 'analysis_preparation', 'id_analysis_preparation', 5, 'hola', false),
+    Array(3, 'analysis_preparation', 'id_analysis_preparation', 6, '45698bca', false),
+    Array(3, 'analysis_preparation', 'id_analysis_preparation', 7, '7894', false),
+    Array(3, 'analysis_preparation', 'id_analysis_preparation', 8, 789465, true),
+    //name_analysis_preparation
+    //description_analysis_preparation
+    //bib_analysys_preparation
+    //file analysys_preparation
     
+    //project
+    //id_project
+    //name_project
+    //start_date_project
+    //end_date_project
+    //resonsable_project
+    //organization_project
+    //description_project
+    //fie_project
+    //code_project
+    //acronym_project
+    //id_sampling_methodology
+
 );
